8b10cad547a08eeebdcdbf9eb031455652d6fda6
kawax <kawaxbiz@gmail.com>
2023-03-03 11:22:01 +0900

    Update Factory.php

--- src/Contracts/Factory.php
+++ src/Contracts/Factory.php
@@ -2,8 +2,6 @@
 
 namespace Revolution\Google\Sheets\Contracts;
 
-use Google\Service;
-use Google\Service\Drive;
 use Google\Service\Sheets;
 use Google\Service\Sheets\AppendValuesResponse;
 use Google\Service\Sheets\BatchUpdateSpreadsheetResponse;
@@ -13,64 +11,27 @@ use Illuminate\Support\Collection;
 
 interface Factory
 {
-    /**
-     * @param  Service|Sheets  $service
-     * @return $this
-     */
-    public function setService(Service|Sheets $service): static;
+    public function setService(mixed $service): static;
 
-    /**
-     * @return Sheets
-     */
     public function getService(): Sheets;
 
     /**
      * set access_token and set new service.
-     *
-     * @param  array|string  $token
-     * @return $this
      */
     public function setAccessToken(array|string $token): static;
 
-    /**
-     * @param  string  $spreadsheetId
-     * @return $this
-     */
     public function spreadsheet(string $spreadsheetId): static;
 
-    /**
-     * @param  string  $title
-     * @return $this
-     */
     public function spreadsheetByTitle(string $title): static;
 
-    /**
-     * @param  string  $sheet
-     * @return $this
-     */
     public function sheet(string $sheet): static;
 
-    /**
-     * @param  string  $sheetId
-     * @return $this
-     */
     public function sheetById(string $sheetId): static;
 
-    /**
-     * @return array
-     */
     public function sheetList(): array;
 
-    /**
-     * @return \Illuminate\Support\Collection
-     */
     public function get(): Collection;
 
-    /**
-     * @param  array  $header
-     * @param  array|\Illuminate\Support\Collection  $rows
-     * @return \Illuminate\Support\Collection
-     */
     public function collection(array $header, array|Collection $rows): Collection;
 
     /**
@@ -78,94 +39,39 @@ interface Factory
      */
     public function spreadsheetList(): array;
 
-    /**
-     * @param  Drive|Service  $drive
-     * @return $this
-     */
-    public function setDriveService(Service|Drive $drive): static;
+    public function setDriveService(mixed $drive): static;
 
-    /**
-     * @return Drive|Service
-     */
-    public function getDriveService(): Service|Drive;
+    public function getDriveService(): mixed;
 
-    /**
-     * @return object
-     */
     public function spreadsheetProperties(): object;
 
-    /**
-     * @return object
-     */
     public function sheetProperties(): object;
 
-    /**
-     * @return array
-     */
     public function all(): array;
 
-    /**
-     * @return array
-     */
     public function first(): array;
 
-    /**
-     * @param  array  $value
-     * @param  string  $valueInputOption
-     * @return BatchUpdateValuesResponse
-     */
     public function update(array $value, string $valueInputOption = 'RAW'): BatchUpdateValuesResponse;
 
-    /**
-     * @return ClearValuesResponse|null
-     */
     public function clear(): ?ClearValuesResponse;
 
-    /**
-     * @param  array  $value
-     * @param  string  $valueInputOption
-     * @param  string  $insertDataOption
-     * @return AppendValuesResponse
-     */
-    public function append(array $value, string $valueInputOption = 'RAW', string $insertDataOption = 'OVERWRITE'): AppendValuesResponse;
+    public function append(
+        array $value,
+        string $valueInputOption = 'RAW',
+        string $insertDataOption = 'OVERWRITE',
+    ): AppendValuesResponse;
 
-    /**
-     * @return string|null
-     */
     public function ranges(): ?string;
 
-    /**
-     * @param  string  $range
-     * @return $this
-     */
     public function range(string $range): static;
 
-    /**
-     * @param  string  $majorDimension
-     * @return $this
-     */
     public function majorDimension(string $majorDimension): static;
 
-    /**
-     * @param  string  $dateTimeRenderOption
-     * @return $this
-     */
     public function dateTimeRenderOption(string $dateTimeRenderOption): static;
 
-    /**
-     * @return string
-     */
     public function getSpreadsheetId(): string;
 
-    /**
-     * @param  string  $sheetTitle
-     * @return BatchUpdateSpreadsheetResponse
-     */
     public function addSheet(string $sheetTitle): BatchUpdateSpreadsheetResponse;
 
-    /**
-     * @param  string  $sheetTitle
-     * @return BatchUpdateSpreadsheetResponse
-     */
     public function deleteSheet(string $sheetTitle): BatchUpdateSpreadsheetResponse;
 }
