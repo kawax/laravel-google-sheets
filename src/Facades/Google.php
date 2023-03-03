3afd0829cac8a0e227ed848392b68a1fd01fbda2
kawax <kawaxbiz@gmail.com>
2023-03-03 11:07:43 +0900

    Update Google.php

--- src/Facades/Google.php
+++ src/Facades/Google.php
@@ -2,10 +2,18 @@
 
 namespace Revolution\Google\Sheets\Facades;
 
+use Google\Service;
 use Illuminate\Support\Facades\Facade;
+use Psr\Cache\CacheItemPoolInterface;
 use Revolution\Google\Sheets\GoogleSheetClient;
 
 /**
+ * @method static Service make(string $service)
+ * @method static void setAccessToken(array|string $token)
+ * @method static bool isAccessTokenExpired()
+ * @method static void fetchAccessTokenWithRefreshToken(?string $refreshToken = null)
+ * @method static CacheItemPoolInterface getCache()
+ *
  * @mixin GoogleSheetClient
  */
 class Google extends Facade
